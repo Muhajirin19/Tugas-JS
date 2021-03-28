4db74a772a28f6df4c3e66ee19ba64e76c149201
Muhajirin19 <79311378+Muhajirin19@users.noreply.github.com>
2021-03-28 21:01:59 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-	var x = 10;
-	console.log('Hello World');
-	console.log('Isi dari variabel xadlaah ' + x);
+alert('Selamat Datang');
+var lagi = true;
+
+
+while( lagi ) {
+	var nama = prompt('masukkan nama:');
+	alert('hallo' +nama);
+
+	lagi = confirm('coba lagi ?');
+}
+
+alert('terimakasih');
